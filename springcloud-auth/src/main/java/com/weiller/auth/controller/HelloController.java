9ec7dd8c9bda60f15f2f2c119faf47bcea5d3abf
xiweile <xiweile@foresee.com>
2019-03-06 16:22:53 +0800

    测试

--- springcloud-auth/src/main/java/com/weiller/auth/controller/HelloController.java
+++ springcloud-auth/src/main/java/com/weiller/auth/controller/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ import reactor.core.publisher.FluxOperator;
 import java.util.*;
 
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Autowired
